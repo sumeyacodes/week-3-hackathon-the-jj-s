c6fd3966b90a7eac5c0497839f936bf304b3f0cb
Jackafella1 <jackafella@gmail.com>
2024-12-12 13:38:59 +0000

    js and HTML

---
+++ script.js
@@ -0,0 +1,5 @@
+//we need an async 08 function for fetching the API
+//make a function for displaying the meme
+//function for random generator button.
+//add an eventListener for the image. 
+//add an eventListener for the button.
