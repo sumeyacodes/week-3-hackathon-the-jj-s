a0e7959262bc17be667ce2adc2162143ce7973b5
sumeyacodes <sumeya4h@gmail.com>
2024-12-12 16:44:15 +0000

    subreddit api added

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const generateMemeBtn = document.getElementById("meme-button")
 //we need an async await function for fetching the API
 async function fetchMemeApi() {
     try {
-        const url = "https://meme-api.com/gimme";
+        const subreddit = 'ProgrammerHumor'
+        const url = `https://meme-api.com/gimme/${subreddit}`;
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
         
+        // refractor this into seperate function
             memeTitle.innerText = data.title;
             memeImage.src = data.url;
             memeImage.alt = data.title;
@@ -23,15 +25,3 @@ async function fetchMemeApi() {
 
 window.addEventListener("DOMContentLoaded", fetchMemeApi)
 generateMemeBtn.addEventListener("click", fetchMemeApi);
-
-
-//fetchMemeApi();
-//make a function for displaying the meme
-
-//function for random generator button.
-//add an eventListener for the image. 
-//add an eventListener for the button.
-
-//Things to fix
-    // nsfw memes still appearing randomly
-    // add loading page or any loading
