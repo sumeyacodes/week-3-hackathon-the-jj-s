1154ba53189c83ef22d9f0e12e43cf31e53c196d
Jackafella1 <jackafella@gmail.com>
2024-12-12 13:54:03 +0000

    fetch function added for API

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 //we need an async 08 function for fetching the API
+async function fetchMemeApi() {
+    const url = "https://meme-api.com/gimme";
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+
+}
+fetchMemeApi();
 //make a function for displaying the meme
 //function for random generator button.
 //add an eventListener for the image. 
