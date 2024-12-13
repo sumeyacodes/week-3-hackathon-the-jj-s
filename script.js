fcb89a006cf9e2dca689744aa5e107f27f67f700
sumeyacodes <sumeya4h@gmail.com>
2024-12-13 10:03:46 +0000

    added in error cases, loading state, random subreddit array function, & comments to expl ain code

--- script.js
+++ script.js
@@ -1,30 +1,80 @@
+//Initialise variables from HTML
 const memeTitle = document.getElementById("meme-title");
 const memeImage = document.getElementById("meme-image");
 const generateMemeBtn = document.getElementById("meme-button");
+const loadingIndicator = document.getElementById("loading")
+const memeCountDisplay = document.getElementById("meme-count")
+
+//Start meme count from 0
+let memeCount = 0
+
+//Get a random subreddit from array to get meme from
+function getRandomSubreddit() {
+    const subredditsArray = [
+    'ProgrammerHumour',
+    'programmingmemes',
+    ' codingmemes',
+    'devhumormemes',
+    'programminghumor'
+    ];
+
+    const randomIndex = Math.floor(Math.random () * subredditsArray.length);
+
+    return subredditsArray[randomIndex];
+}
+
 
 //Function for fetching the API
 async function fetchMemeAPI() {
     try {
-        const subreddit = 'ProgrammerHumor'
+
+        //Display loading state
+        loadingIndicator.style.display = 'block'
+
+        //Get random subreddit
+        const subreddit = getRandomSubreddit()
+        
+        //Fetch data from API
         const url = `https://meme-api.com/gimme/${subreddit}`;
         const response = await fetch(url);
         const data = await response.json();
+
+        // Filtering out nsfw memes
+        if(data.nsfw){
+            console.log("nsfw found")
+            fetchMemeAPI()
+            return
+        }
+
         return data
     }
 
     catch (error) {
+
+        //Handing error cases
         console.error(`ERROR: ${error.message}`);
-        return null;
+        memeTitle.innerText = "Something went wrong, cannot retrieve meme"
+        memeImage.src = ""
+        memeImage.alt = "Error loading meme"
+    }
+
+    finally {
+        // After loaded, loading indicator will disappear 
+        loadingIndicator.style.display = "none"
     }
 }
-//Function for updating DOM
 
+//Function for updating DOM
 function displayMeme(memeData){
+
+    //Update meme data
     memeTitle.innerText = memeData.title;
     memeImage.src = memeData.url;
     memeImage.alt = memeData.title;
 
-    console.log(memeData)
+    //Update & display meme count when generated
+    memeCount++
+    memeCountDisplay.innerText = `Memes Generated: ${memeCount}`
 }
 
 //Function to pass api data to display meme function
@@ -33,5 +83,8 @@ async function generateAndDisplayNewMeme() {
     displayMeme(memeData);
 }
 
+//To display meme when page is refreshed
 window.addEventListener("DOMContentLoaded", generateAndDisplayNewMeme)
+
+//Generate new meme when button is clicked
 generateMemeBtn.addEventListener("click", generateAndDisplayNewMeme);
