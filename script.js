68e5727b295155726ed6159687de9f355ae77e5f
sumeyacodes <sumeya4h@gmail.com>
2024-12-15 03:08:54 +0000

    local storage added, working version only shows new urls

--- script.js
+++ script.js
@@ -9,12 +9,13 @@ const memeCountDisplay = document.getElementById("meme-count");
 let generatedMemeCount = 0;
 let memeCount = 0;
 
-let prefetchedMemes = [];
-let seenMemes = [];
-let newMemes = [];
 
 // Retrieve memes from local storage and store in seen memes array, if nothing initialise new array
-console.log('seen memes array', seenMemes)
+let seenMemes = JSON.parse(localStorage.getItem('seenMemes')) || [];
+let prefetchedMemes = [];
+
+
+
 
 // ** COMMENTED OUT FOR NOW TO TEST FUNCTIONS WITH JUST ONE SUBREDDIT ** //
 //Get a random subreddit from array to get meme from
@@ -62,45 +63,66 @@ async function fetchMemeAPI(amountLeftToFetch) {
 }
 
 /*
-1. retrieve 10 items from api and store in temporary array
-2. find the ones that don't match my items from local storage
+1. retrieve 10 items from api 
+2. find the ones that don't match my items from seen array (first, then change to local storage)
 3. push new items into new memes array
 4. when button is clicked, new meme is retrieved from array and displayed on DOM
 5. when displayed on DOM - meme is removed from new memes array and put into seen local storage array
 6. but then how do i make it retrieve the remaining to make 10
 7. i guess if i store this in a function, i can recall the fetch meme api and pass in the remaining numbers left from the new memes array ? 
 8. store that in count so it passes it as a query to the api, and that process will keep repeating until it reaches 10
-9. and will keep refetching everytime there is less than 10 in new memes array
+9. and will keep refetching everytime there is less than 10 in new memes array ??
 */
 
 async function prefetchData() {
-  // Fetch 10 meme objects from api 
-  const { memes } = await fetchMemeAPI(10);
-  console.log('Meme array fetched from API:', memes);
+  // Calculate how many more memes are needed
+  const remainingMemes = 10 - prefetchedMemes.length;
 
-  // Check if the meme is not in the seenMemes array (by comparing url)
-  memes.forEach(meme => {
-    const isSeen = seenMemes.some(seenItem => seenItem === meme.url);
 
-    // If the meme is not in the seenMemes array, push it to the prefetchedMemes array
-    if (!isSeen) {
-      prefetchedMemes.push(meme);
-    }
-  });
+  // Fetch remaining memes to make up 10
+  if (remainingMemes > 0) {
+
+    const { memes } = await fetchMemeAPI(remainingMemes);
+    console.log('Fetched memes from API:', memes);
+
+    // Check if the meme is not in the seenMemes array (by comparing url)
+    memes.forEach(meme => {
+      const isSeen = seenMemes.some(seenItem => seenItem === meme.url);
+
+      // If the meme is not in the seenMemes array, push it to the prefetchedMemes array
+      if (!isSeen) {
+        prefetchedMemes.push(meme);
+      }
+    })}
+
+  // set seen memes to local storage
+  localStorage.setItem('seenMemes array', JSON.stringify(seenMemes))
   console.log(seenMemes)
+
   console.log('Prefetched items array:', prefetchedMemes);
 
-  // If there are prefetched memes, return the last one
+
+
   if (prefetchedMemes.length > 0) {
-    let memeToDisplay = prefetchedMemes[prefetchedMemes.length - 1];
 
-    // Remove the last item from prefetchedMemes to not repeat display
-    prefetchedMemes.pop();
-    seenMemes.push(memeToDisplay)
-
+    // Get the last meme from the prefetchedMemes array
+    let memeToDisplay = prefetchedMemes.pop();  // Pop the last meme to display
     console.log('Meme to display:', memeToDisplay);
+
+    // After displaying the meme, add it to seenMemes
+    seenMemes.push(memeToDisplay.url);
+
+    // Store the updated seenMemes back to localStorage
+    localStorage.setItem('seenMemes', JSON.stringify(seenMemes));
+
+
+    // Return the meme to be displayed
     return memeToDisplay;
-  } 
+  } else {
+    console.log("There are no more new memes, get a life")
+    memeTitle.innerText =  "User needs to touch grass"
+    memeCountDisplay.innerText = "There are no more new memes, get a life"
+  }
 }
 
 //Function for updating DOM
@@ -115,7 +137,7 @@ function displayMeme(title, url) {
   memeCountDisplay.innerText = `Memes Generated: ${generatedMemeCount}`;
 }
 
-//Function to pass api data to display meme function
+//Function button calls to pass api data to display meme function
 async function generateAndDisplayNewMeme() {
   const {title, url} = await prefetchData()
 
