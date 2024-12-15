a3210671d0487362c96d3d955a8c3f2c83409a01
sumeyacodes <sumeya4h@gmail.com>
2024-12-15 01:22:18 +0000

    changing logic: created prefetched data, finding memes that aren't in seen memes array and storing them in prefetched array & sending that data to generate and display new meme function

--- script.js
+++ script.js
@@ -6,50 +6,46 @@ const loadingIndicator = document.getElementById("loading");
 const memeCountDisplay = document.getElementById("meme-count");
 
 //Start meme count from 0
+let generatedMemeCount = 0;
 let memeCount = 0;
 
-//Get a random subreddit from array to get meme from
-function getRandomSubreddit() {
-  const subredditsArray = [
-    "ProgrammerHumour",
-    "programmingmemes",
-    " codingmemes",
-    "devhumormemes",
-    "programminghumor",
-  ];
-  const randomIndex = Math.floor(Math.random() * subredditsArray.length);
-  return subredditsArray[randomIndex];
-}
-
+let prefetchedMemes = [];
 let seenMemes = [];
+let newMemes = [];
 
+// Retrieve memes from local storage and store in seen memes array, if nothing initialise new array
+console.log('seen memes array', seenMemes)
+
+// ** COMMENTED OUT FOR NOW TO TEST FUNCTIONS WITH JUST ONE SUBREDDIT ** //
+//Get a random subreddit from array to get meme from
+// function getRandomSubreddit() {
+//   const subredditsArray = [
+//     "ProgrammerHumour",
+//     "programmingmemes",
+//     " codingmemes",
+//     "devhumormemes",
+//     "programminghumor",
+//   ];
+//   const randomIndex = Math.floor(Math.random() * subredditsArray.length);
+//   return subredditsArray[randomIndex];
+// }
 
 //Function for fetching the API
-async function fetchMemeAPI() {
+async function fetchMemeAPI(amountLeftToFetch) {
   try {
     //Display loading state
     loadingIndicator.style.display = "block";
 
     //Get random subreddit
-    const subreddit = getRandomSubreddit();
+    const subreddit =  "codingmemes"
+
+    // Meme count to pass in as query
+    memeCount = amountLeftToFetch
 
     //Fetch data from API
-    const url = `https://meme-api.com/gimme/${subreddit}`;
+    const url = `https://meme-api.com/gimme/${subreddit}/${memeCount}`;
     const response = await fetch(url);
     const data = await response.json();
-
-    // Filtering out nsfw memes
-    if (data.nsfw) {
-      console.log("nsfw found");
-      return fetchMemeAPI()
-    }
-
-    if (!data.title || !data.url) {
-      console.log(
-        "Meme with undefined or empty title/url found. Fetching another..."
-      );
-      return fetchMemeAPI();
-    }
 
     return data;
 
@@ -65,44 +61,66 @@ async function fetchMemeAPI() {
   }
 }
 
+/*
+1. retrieve 10 items from api and store in temporary array
+2. find the ones that don't match my items from local storage
+3. push new items into new memes array
+4. when button is clicked, new meme is retrieved from array and displayed on DOM
+5. when displayed on DOM - meme is removed from new memes array and put into seen local storage array
+6. but then how do i make it retrieve the remaining to make 10
+7. i guess if i store this in a function, i can recall the fetch meme api and pass in the remaining numbers left from the new memes array ? 
+8. store that in count so it passes it as a query to the api, and that process will keep repeating until it reaches 10
+9. and will keep refetching everytime there is less than 10 in new memes array
+*/
 
+async function prefetchData() {
+  // Fetch 10 meme objects from api 
+  const { memes } = await fetchMemeAPI(10);
+  console.log('Meme array fetched from API:', memes);
+
+  // Check if the meme is not in the seenMemes array (by comparing url)
+  memes.forEach(meme => {
+    const isSeen = seenMemes.some(seenItem => seenItem === meme.url);
+
+    // If the meme is not in the seenMemes array, push it to the prefetchedMemes array
+    if (!isSeen) {
+      prefetchedMemes.push(meme);
+    }
+  });
+  console.log(seenMemes)
+  console.log('Prefetched items array:', prefetchedMemes);
+
+  // If there are prefetched memes, return the last one
+  if (prefetchedMemes.length > 0) {
+    let memeToDisplay = prefetchedMemes[prefetchedMemes.length - 1];
+
+    // Remove the last item from prefetchedMemes to not repeat display
+    prefetchedMemes.pop();
+    seenMemes.push(memeToDisplay)
+
+    console.log('Meme to display:', memeToDisplay);
+    return memeToDisplay;
+  } 
+}
 
 //Function for updating DOM
-function displayMeme(memeData) {
+function displayMeme(title, url) {
   //Update meme data
-  memeTitle.innerText = memeData.title;
-  memeImage.src = memeData.url;
-  memeImage.alt = memeData.title;
+  memeTitle.innerText = title;
+  memeImage.src = url;
+  memeImage.alt = title;
 
   //Update & display meme count when generated
-  memeCount++;
-  memeCountDisplay.innerText = `Memes Generated: ${memeCount}`;
+  generatedMemeCount++;
+  memeCountDisplay.innerText = `Memes Generated: ${generatedMemeCount}`;
 }
 
 //Function to pass api data to display meme function
 async function generateAndDisplayNewMeme() {
-  const memeData = await fetchMemeAPI()
+  const {title, url} = await prefetchData()
 
-  //Find repeated meme in seenMemes array
- let repatedMeme = seenMemes.find(url => url === memeData.url);
-
- //If found a repeated meme, trigger function get new meme
- if (repatedMeme) {
-   console.log("Meme retrieved:", memeData.url); 
-   console.log("Repeated meme found:", repatedMeme); 
-   console.log("Memes Seen so far:", seenMemes);
-   console.log("Skipping this meme and fetching a new one...");
-   generateAndDisplayNewMeme();
-   return;
- }
-
-  // Add new URL to seenMemes array
-  seenMemes.push(memeData.url);
-  console.log("New meme added to seenMemes:", memeData.url);
-
-  // Display the new meme
-  displayMeme(memeData);
-  console.log("FALSE: Displaying new meme:", memeData.url);
+  // pass destructured data to display 
+  displayMeme(title, url);
 }
 
 //To display meme when page is refreshed
